64d71d27272536bf818ec37ac706bfda8bf0a774
mbsii3 <mshortjr3@gmail.com>
2023-02-08 16:25:14 -0600

    Finished the play again button and finished the reset game functions.  Added a background to the webpage and to the back of the cards.

--- app.js
+++ app.js
@@ -18,27 +18,17 @@ const getImgs = [
     {imgSrc: "./images/tengen.jpg", name: "tengen"},
     {imgSrc: "./images/zenitsu.jpg", name: "zenitsu"}
 ];
-
-
 const cardTable = document.querySelector('section');
 const playerTurnsCount = document.getElementById('turnCount');
 const matchesCount = document.getElementById('matchCount');
-
-
-
 /*----- state variables -----*/
 let turns;
 let matches;
-
-
 /*----- cached elements  -----*/
 const messageEl = document.getElementById('win-message');
 const playAgainBtn = document.querySelector('button');
-
-
 /*----- event listeners -----*/
-playAgainBtn.addEventListener('click', init);
-
+playAgainBtn.addEventListener('click', newGame);
 /*----- functions -----*/
 init();
 
@@ -50,15 +40,20 @@ function init() {
     render();
 }
 
+function newGame() {
+    resetCards();
+    setTimeout(() => reshuffle(), 1500);
+    setTimeout(() => resetPointer(), 1500);
+    restart();
+}
+
 function render() {
     renderGame();
-    // renderButton();
 }
 
 // Function randomizes the card images in the getImgs array
 // and generates the HTML elements for the cards and appends them to the DOM
-//checkCards function is called at the end
-
+// checkCards function is called at the end
 function renderGame() {
     const randomImgs = getImgs.sort(() => Math.random() - 0.5);
     randomImgs.forEach(randomImg => {
@@ -68,14 +63,15 @@ function renderGame() {
         card.classList = 'card';
         face.classList = 'face';
         back.classList = 'back';
-        //Attach the randomImgs to the face of the cards
+        // Attach the randomImgs to the face of the cards
         face.src = randomImg.imgSrc;
         card.setAttribute('name', randomImg.name);
         // Append the cards to the section element(cardTable) && the images to the card faces
         cardTable.append(card);
         card.append(face);
         card.append(back);
-
+        // Click event listener located here due to the HTML elements being created
+        // locally in this function
         card.addEventListener('click', (flipCard) => {
             card.classList.toggle('toggleCard');
             checkCards(flipCard);
@@ -83,13 +79,12 @@ function renderGame() {
         });
     });
 }
-
-//Check Cards
+// Check Cards
 function checkCards(flipCard) {
     const clickedCard = flipCard.target;
     clickedCard.classList.add('toggled');
     const toggledCards = document.querySelectorAll('.toggled');
-    //Check Cards Logic
+    // Check Cards Logic
     if (toggledCards.length === 2) {
         if (toggledCards[0].getAttribute('name') === toggledCards[1].getAttribute('name')) {
             matches++;
@@ -109,32 +104,49 @@ function checkCards(flipCard) {
         }
     }
 }
-
+// Win/Loss logic
 function checkResult() {
     if (turns === 0 && matches < 8) {
-        setTimeout(() => messageEl.innerText = 'You lose, try again!', 1000);
         resetCards();
+        cardTable.style.pointerEvents = 'none';
+        setTimeout(() => messageEl.innerText = 'You lose, try again!', 1000);
     } else if (matches === 8 && turns > 0) {
-        setTimeout(() => messageEl.innerText = 'You Win!!', 1000);
+        cardTable.style.pointerEvents = 'none';
+        setTimeout(() => messageEl.innerText = 'You Win!', 1000);
     }
 }
-
+// Function flips all cards back to face down
 function resetCards() {
+    let currentImgs = getImgs;
+    let cards = document.querySelectorAll('.card');
+    currentImgs.forEach((img, idx) => {
+        setTimeout(() => cards[idx].classList.remove('toggleCard'), 1000);
+    });
+}
+// Function resets pointer events on all cards and resets the name attribute
+// after being reshuffled
+function resetPointer() {
     let currentImgs = getImgs;
     let faces = document.querySelectorAll('.face');
     let cards = document.querySelectorAll('.card');
     currentImgs.forEach((img, idx) => {
-        setTimeout(() => cards[idx].classList.remove('toggleCard'), 1000);
         cards[idx].style.pointerEvents = 'all';
         faces[idx].src = img.imgSrc;
+        cards[idx].setAttribute('name', img.name);
     });
-        // turns = 3;
-        // playerTurnsCount.textContent = turns;
 }
-
-// function renderButton() {
-//     playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
-// }
+// Function reshuffles the images on the card faces
+function reshuffle() {
+    getImgs.sort(() => Math.random() - 0.5);
+}
+// Resets game info
+function restart() {
+    turns = 8;
+    matches = 0;
+    playerTurnsCount.textContent = turns;
+    matchesCount.textContent = matches;
+    messageEl.innerText = 'Match the cards!';
+}
     
 
 
